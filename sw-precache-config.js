5a12c4250629edbea241e0633c68fb2a8145a2f5
Arthur <pyeye.91@gmail.com>
2017-07-13 13:57:14 +0500

    prepare to rel

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,11 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/lightgallery.js/dist/css/lightgallery.min.css',
+    '/bower_components/lightgallery.js/dist/js/lightgallery.min.js',
+    '/bower_components/lg-thumbnail.js/dist/lg-thumbnail.min.js',
+    '/bower_components/lg-hash.js/dist/lg-hash.min.js',
   ],
   navigateFallback: '/index.html'
 };
